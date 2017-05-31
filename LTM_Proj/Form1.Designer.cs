0c309667e2798f0aebd298f160ec9c88f42afb5d
znqt <z.nqt225@gmail.com>
2017-05-31 19:34:24 +0700

    Project Backup Servers

---
+++ LTM_Proj/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace LTM_Proj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvServers = new System.Windows.Forms.ListView();
+            this.colServers = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colIP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colPort = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colStatus = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnDiscnt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvServers
+            // 
+            this.lvServers.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colServers,
+            this.colIP,
+            this.colPort,
+            this.colStatus});
+            this.lvServers.Location = new System.Drawing.Point(12, 12);
+            this.lvServers.Name = "lvServers";
+            this.lvServers.Size = new System.Drawing.Size(490, 625);
+            this.lvServers.TabIndex = 0;
+            this.lvServers.UseCompatibleStateImageBehavior = false;
+            this.lvServers.View = System.Windows.Forms.View.Details;
+            // 
+            // colServers
+            // 
+            this.colServers.Text = "Servers";
+            this.colServers.Width = 53;
+            // 
+            // colIP
+            // 
+            this.colIP.Text = "IP address";
+            this.colIP.Width = 217;
+            // 
+            // colPort
+            // 
+            this.colPort.Text = "Port";
+            this.colPort.Width = 95;
+            // 
+            // colStatus
+            // 
+            this.colStatus.Text = "Status";
+            this.colStatus.Width = 121;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(531, 28);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(122, 114);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnDiscnt
+            // 
+            this.btnDiscnt.Location = new System.Drawing.Point(531, 160);
+            this.btnDiscnt.Name = "btnDiscnt";
+            this.btnDiscnt.Size = new System.Drawing.Size(122, 115);
+            this.btnDiscnt.TabIndex = 2;
+            this.btnDiscnt.Text = "DISCONNECT";
+            this.btnDiscnt.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 649);
+            this.Controls.Add(this.btnDiscnt);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lvServers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColumnHeader colIP;
+        private System.Windows.Forms.ColumnHeader colPort;
+        private System.Windows.Forms.ColumnHeader colStatus;
+        private System.Windows.Forms.ColumnHeader colServers;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnDiscnt;
+        protected System.Windows.Forms.ListView lvServers;
+
+
+    }
+}
+
---
+++ LTM_Proj/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace LTM_Proj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        Root ROOT = new Root();
+        private Object thislock = new Object();
+        delegate void UpdateLV();
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            ROOT.Start();
+          //  Thread t = new Thread(DisplayListView);
+            //t.Start();
+            DisplayListView();
+        }
+
+        private void DisplayListView()
+        {
+            while (true)
+            {
+                if (this.lvServers.InvokeRequired)
+                {
+                    UpdateLV d = new UpdateLV(DisplayListView);
+                    this.Invoke(d);
+                }
+                else
+                {
+                     int j = 1;
+                    foreach (Server i in ROOT.GetListServers())
+                    {
+                        if (i != null)
+                        {
+                            string[] str = { j.ToString(), i.Ip, i.Port, i.status };
+                            ListViewItem item = new ListViewItem(str);
+                            lvServers.Items.Add(item);
+                            j++;
+                        }
+                    }
+                }
+            }
+        }
+       
+    }
+    public class Server
+    {
+        public string Ip{get;set;}
+        public string Port { get; set; }
+        public string status{get;set;}
+        public Server(string _ip,string _port)
+        {
+            Ip = _ip;
+            Port = _port;
+            status = "-1";
+        }
+    }
+    public class Root
+    {
+        public static int NumbersOfServers;
+        public static IPAddress ipad;
+        public static int port,portClient;
+        public static TcpListener tcpRoot,tcpForClient;
+        public static int count = 0;
+        public static int count2 = 0;
+        public static Socket[] socket;
+        public static Socket[] clientSock;
+        public static Server[] ListServers;
+        
+        private static Object lock1 = new Object();
+        private static Object lock2 = new Object();
+        public Root(int n=100)
+        {
+            NumbersOfServers = n;
+            ListServers = new Server[NumbersOfServers];
+       
+        }
+        public void Start()
+        {
+            try
+            {
+                //Init
+                socket = new Socket[NumbersOfServers];
+                clientSock = new Socket[100];
+                ipad = IPAddress.Parse("127.0.0.1");
+                port = 12000;
+                portClient = 13000;
+                tcpRoot = new TcpListener(ipad, port);
+                tcpForClient = new TcpListener(ipad, portClient);
+                //Listen
+                tcpRoot.Start();
+                tcpForClient.Start();
+                //
+                Thread threadServer = new Thread(HandleServer);
+                Thread threadClient = new Thread(HandleClient);
+                threadServer.Start();
+                threadClient.Start();
+                
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.StackTrace);
+            }
+        }
+        public void Disconnect()
+        {
+            tcpRoot.Stop();
+            tcpForClient.Stop();
+        }
+        public Server[] GetListServers()
+        {
+            return ListServers;
+        }
+        static void HandleServer()
+        {
+            
+            while (true)
+            {
+               
+                socket[count] = tcpRoot.AcceptSocket();
+                //lock
+                if (count > NumbersOfServers)
+                {
+                    count = 0;
+                }
+                else
+                {
+                    count++;
+                }
+                Thread t = new Thread(WorkerServer);
+                t.Start(count - 1);
+            }
+        }
+        static void HandleClient()
+        {
+            while (true)
+            {
+                clientSock[count2] = tcpForClient.AcceptSocket();
+                //lock
+                if (count2 > 100)
+                {
+                    count2 = 0;
+                }
+                else
+                {
+                    count2++;
+                }
+                Thread t = new Thread(WorkerClient);
+                t.Start(count2 - 1);
+            }
+        }
+
+        private static int IndexOfServ(Server a)
+        {
+            lock (lock1)
+            {
+                for (int i = 0; i < NumbersOfServers; i++)
+                {
+                    if (ListServers[i] == a)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+        static void WorkerServer(object obj)
+        {
+            int index = (Int32)obj;
+            byte[] recv = new byte[100];
+            lock (lock1)
+            {
+                try
+                {
+                    int k = socket[index].Receive(recv);
+                    string Data = "";
+                    for (int i = 0; i < k; i++)
+                    {
+                        Data += Convert.ToChar(recv[i]);
+                    }
+                    int j = Data.IndexOf(":");
+                    int t = Data.IndexOf(";");
+                    string servIp = Data.Substring(0, j);
+                    string servPort = Data.Substring(j + 1, t - 1 - j);
+                    string Status = Data.Substring(t + 1, Data.Length - t - 1);
+                    Server tmp = new Server(servIp, servPort);
+                    j = IndexOfServ(tmp);
+                    ListServers[index] = tmp;
+                    ListServers[index].status = Status;
+                }
+                catch (Exception err)
+                {
+                    socket[index].Close();
+                    //ListServers[index].status = "-1";
+                }
+                socket[index].Close();
+                
+            }
+
+        }
+        private static int FindServerFree()
+        {
+            for (int i = 0; i < NumbersOfServers; i++)
+            {
+                if (ListServers[i].status == "0")
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        static void WorkerClient(object obj)
+        {
+            int inx = (Int32)obj;
+            byte[] recv = new byte[1];
+            byte[] send = new byte[20];
+            lock (lock2)
+            {
+                clientSock[inx].Receive(recv);
+                if (recv != null)
+                {
+                    int f;
+                    Server free;
+                    string str="";
+                    lock (lock1)
+                    {
+                        lock (lock1)
+                        {
+                            f = FindServerFree();
+                        }
+                        if (f != -1)
+                        {
+                            free = ListServers[f];
+                            str = free.Ip+":"+free.Port;
+                            ListServers[f].status = "1";
+                        }
+                    }
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    clientSock[inx].Send(asen.GetBytes(str));
+                }
+                clientSock[inx].Close();
+            }
+        }
+    }
+}
