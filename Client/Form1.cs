26d6676f73300ff17afb313b626e1896c54444f8
znqt <z.nqt225@gmail.com>
2017-06-01 23:39:27 +0700

    update reconnect

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Client
 {
@@ -16,39 +17,53 @@ namespace Client
         public Form1()
         {
             InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
         }
+        private double TIMEOUT = 30;
         public static string servIp, servPort;
+        public static string currIp, currPort;
+        private Object lock1 = new Object();
+        private TcpClient tcpclnt;
+        private DateTime lasttime = DateTime.Now;
+        Stream stm;
+        private void ReqAndResp()
+        {
+            tcpclnt = new TcpClient();
+            byte[] byteSend;
+            byte[] byteReceive;
 
+            tcpclnt.Connect(currIp, int.Parse(currPort));
+            tcpclnt.ReceiveTimeout = 15000;
+            stm = tcpclnt.GetStream();
+            ASCIIEncoding asen = new ASCIIEncoding();
+            string send = tbNuma.Text + ";" + tbNumb.Text;
+            byteSend = asen.GetBytes(send);
+            stm.Write(byteSend, 0, byteSend.Length);
+
+            byteReceive = new byte[100];
+            int k = stm.Read(byteReceive, 0, 100);
+            string recv = System.Text.Encoding.UTF8.GetString(byteReceive);
+            tbResult.Text = recv;
+            tcpclnt.Close();
+        }
         private void btnConnectServer_Click(object sender, EventArgs e)
         {
             try
             {
-                TcpClient tcpclnt = new TcpClient();
-                Stream stm;
-                byte[] byteSend;
-                byte[] byteReceive;
-
-                tcpclnt.Connect(servIp, int.Parse(servPort));
-
-                stm = tcpclnt.GetStream();
-                ASCIIEncoding asen = new ASCIIEncoding();
-                string send = tbNuma.Text + ";" + tbNumb.Text;
-                byteSend = asen.GetBytes(send);
-                stm.Write(byteSend, 0, byteSend.Length);
-
-                byteReceive = new byte[100];
-                int k = stm.Read(byteReceive, 0, 100);
-                string recv = System.Text.Encoding.UTF8.GetString(byteReceive);
-                tbResult.Text = recv;
-                tcpclnt.Close();
+                ReqAndResp();
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.ToString());
+                MessageBox.Show("Current Server was shutdown.\nConnecting another Server...");
+                Thread b = new Thread(ConnectRoot);
+                b.IsBackground = true;
+                b.Start();
+                b.Join();
+                ReqAndResp();
             }
         }
 
-        private void btnConnectRoot_Click(object sender, EventArgs e)
+        private void ConnectRoot()
         {
             TcpClient tcpclnt = new TcpClient();
             Stream stm;
@@ -66,19 +81,79 @@ namespace Client
             byteReceive = new byte[100];
             int k = stm.Read(byteReceive, 0, 100);
             string Data = System.Text.Encoding.UTF8.GetString(byteReceive);
-            if (Data.Substring(0,4).Equals("FULL"))
+            if (Data.Substring(0, 4).Equals("FULL"))
             {
                 MessageBox.Show("Server Full!");
                 tcpclnt.Close();
                 return;
             }
             int j = Data.IndexOf(":");
-            servIp = Data.Substring(0, j);
-            servPort = Data.Substring(j + 1, k - 1 - j);
-            lbIP.Text = servIp;
-            lbPORT.Text = servPort;
-
+            currIp = Data.Substring(0, j);
+            currPort = Data.Substring(j + 1, k - 1 - j);
+            lock (lock1)
+            {
+                lbIP.Text = currIp;
+                lbPORT.Text = currPort;
+            }
+            if (servIp == null && servPort==null)
+            {
+                servIp = currIp;
+                servPort = currPort;
+            }
+            lasttime = DateTime.Now;
             tcpclnt.Close();
+            Thread t = new Thread(SetPortA);
+            t.IsBackground = true;
+            t.Start();
+        }
+
+        private void SetPortA()
+        {
+            while (true)
+            {
+                lock (lock1)
+                {
+                    DateTime currentTime = DateTime.Now;
+                    double t = ((TimeSpan)(currentTime - lasttime)).TotalSeconds;
+                    if (t > TIMEOUT)
+                    {
+                        if (currPort != servPort)
+                        {
+                            if (PingHost(servIp, servPort))
+                            {
+
+                                MessageBox.Show(string.Format("Reconnect to {0}:{1}", servIp, servPort));
+                                currIp = servIp;
+                                currPort = servPort;
+                                lbIP.Text = currIp;
+                                lbPORT.Text = currPort;
+
+                            }
+                        }
+                        lasttime = DateTime.Now;
+                    }
+                }
+            }
+        }
+
+        private bool PingHost(string ip, string port)
+        {
+            try
+            {
+                TcpClient tcpc = new TcpClient(ip, int.Parse(port));
+                tcpc.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private void btnConnectRoot_Click(object sender, EventArgs e)
+        {
+            Thread a = new Thread(ConnectRoot);
+            a.IsBackground = true;
+            a.Start();
         }
     }
 }
